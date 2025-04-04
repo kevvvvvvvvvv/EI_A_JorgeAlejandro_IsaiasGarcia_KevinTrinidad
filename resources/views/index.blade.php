60a54c377b2508a1a819041c7b2c789f403bdf78
Kevin <kevinyahirt@gmail.com>
2025-04-03 23:22:37 -0600

    index modificado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,22 +20,25 @@
 </div>
 
 <div class="salones-container">
-    <h1 class="main-title">¡Salones disponibles!</h1>
+    <h1 class="main-title">¡Salones <span class="highlight-text">disponibles</span>!</h1>
     
     <div class="salon-grid">
         <div class="salon-card">
-            <h2 class="salon-title">Título 1</h2>
-            <p class="salon-location">- Location 1</p>
+            <img src="{{URL::asset('images/salon_1.jpeg')}}" alt="">
+            <h2 class="salon-title">Salón 1</h2>
+            <p class="salon-location"><i class="bi bi-geo-alt-fill"></i> Location 1</p>
         </div>
         
         <div class="salon-card">
-            <h2 class="salon-title">Título 2</h2>
-            <p class="salon-location">- Location 2</p>
+            <img src="{{URL::asset('images/salon_2.jpeg')}}" alt="">
+            <h2 class="salon-title">Salón 2</h2>
+            <p class="salon-location"><i class="bi bi-geo-alt-fill"></i> Location 2</p>
         </div>
         
         <div class="salon-card">
-            <h2 class="salon-title">Título 3</h2>
-            <p class="salon-location">- Location 3</p>
+            <img src="{{URL::asset('images/salon_3.jpg')}}" alt="">
+            <h2 class="salon-title">Salón 3</h2>
+            <p class="salon-location"><i class="bi bi-geo-alt-fill"></i> Location 3</p>
         </div>
     </div>
     
@@ -51,8 +54,8 @@
             <h2 class="cualidad-texto">Elegancia y exclusividad</h2>
         </div>
         
-        <div class="cualidad-card">
-            <h2 class="cualidad-texto">Servicio integral y profesionalismo</h2>
+        <div class="cualidad-card mid-card">
+            <h2 class="cualidad-texto mid-card">Servicio integral y profesionalismo</h2>
         </div>
         
         <div class="cualidad-card">
@@ -60,4 +63,31 @@
         </div>
     </div>
 </div>
+@endsection
+
+
+@section('footer')
+<footer class="footer-container">
+    <div class="footer-column">
+        <h3 class="footer-title">¡Contáctanos!</h3>
+        <div class="contact-info">
+            <p class="contact-item">
+                <i class="bi bi-whatsapp"></i> 1234567890
+            </p>
+            <p class="contact-item">
+                <i class="bi bi-facebook"></i> salon_eventos
+            </p>
+            <p class="contact-item">
+                <i class="bi bi-instagram"></i> salon_eventos
+            </p>
+        </div>
+    </div>
+    
+    <div class="footer-column">
+        <h3 class="footer-title">¡Conócenos!</h3>
+        <p class="address">
+            <i class="bi bi-geo-alt-fill"></i> 10 Avenida Mexico, Morelos, Mexico
+        </p>
+    </div>
+</footer>
 @endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Imperial Hall</title>
     <link rel="stylesheet" href="{{ URL::asset('css/layaout.css'); }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     @yield('header')
 </head>
 <body>
